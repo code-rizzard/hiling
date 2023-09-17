d1f23d475d4be2cad0283773983e21f1a7a5887f
Richard <richard232manansala@gmail.com>
2023-09-17 16:01:40 +0800

    Nice

--- src/commands/start.ts
+++ src/commands/start.ts
@@ -1,5 +1,8 @@
 import { Args, Command, Flags, ux } from "@oclif/core";
+import axios from "axios";
+import chalk from "chalk";
 import { URL } from "url";
+import axiosRetry from "axios-retry";
 
 export default class Start extends Command {
   static description = "Starts the automatic requests.";
@@ -19,32 +22,48 @@ export default class Start extends Command {
   };
 
   public async run(): Promise<void> {
-    const { args, flags } = await this.parse(Start);
+    axiosRetry(axios, { retries: 3 });
 
+    const { args, flags } = await this.parse(Start);
     let url = args.url;
     let timeout = flags.timeout;
 
     if (!url) {
       do {
         try {
-          let n = await ux.prompt("What is the url?", {
+          let n = await ux.prompt(`${chalk.green("What is the url?: ")}`, {
             required: true,
           });
           url = new URL(n);
         } catch {
-          this.log("Invalid URL.");
+          this.log(chalk.red("Invalid URL."));
           continue;
         }
       } while (url == undefined);
     }
 
-    ux.action.start(`[GET] to ${url}`, "nice");
-
+    ux.action.start(`${chalk.blue("[GET]")} to ${chalk.blue(url)}`);
     let totalRequest = 0;
-
-    setInterval(() => {
-      ux.action.status = `${totalRequest} finished.`;
+    let success = 0;
+    let failed = 0;
+    setInterval(async () => {
+      ux.action.status = `${chalk.green(`${success} success`)}. ${chalk.red(
+        `${failed} fails.`
+      )} / ${chalk.cyan(`${totalRequest} total`)} `;
       totalRequest += 1;
+      try {
+        await axios.request({
+          url: url?.toString()!,
+        });
+        success += 1;
+      } catch (err) {
+        if (failed % 50 == 0) {
+          this.log(
+            `${chalk.red(`Failed to request ${url}`)} because of ${err}`
+          );
+        }
+        failed += 1;
+      }
     }, timeout);
   }
 }
